63593b0afe3c7f3d933f1dfe89157f3ae2a005c3
ErykWalter <erykwalter2002@gmail.com>
2023-11-21 20:18:28 +0100

    add: evaluator basics

--- evaluator.py
+++ evaluator.py
@@ -1,17 +1,44 @@
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 class Evaluator:
-    def __init__(self, visited_articles: dict, random_articles: dict):
+    """
+    Evaluate the model on the given data.
+    Parameters:
+        visited_articles (dict): A dictionary of visited articles (in this use case an query), key is a url/article title and value should be a document content.
+        random_articles (dict): A dictionary of random articles (in this use case a 'train' set), key is a url/article title and value should be a document content.
+    """
+
+    def __init__(self, visited_articles: dict, scrapped_articles: dict):
         self.visited_articles = visited_articles
-        self.random_articles = random_articles
+        self.scrapped_articles = scrapped_articles
 
     def evaluate(self):
         """
         Evaluate the model on the given data.
         """
-        pass
+        (query_keys, query_vals), (scrap_keys, scrap_vals) = self.tf_idf()
+        similarities = cosine_similarity(query_vals, scrap_vals)
+
+        # map the similarities to orginal scrapped articles
+        result = {}
+        for i, key in enumerate(query_keys):
+            result[key] = similarities[i]
+
+        # return scrapped articles keys for which similarity is the highest
+        return result, similarities
+
 
     def tf_idf(self):
         """
         Implement the TF-IDF algorithm.
         """
+        vectorizer = TfidfVectorizer()
+        scrapped_articles_keys, scrapped_articles_values = zip(*self.scrapped_articles.items())
+        scrapped_articles_vectorized = vectorizer.fit_transform(scrapped_articles_values)
+        
+        visited_articles_keys, visited_articles_values = zip(*self.visited_articles.items())
+        visited_articles_vectorized = vectorizer.transform(visited_articles_values)
+        return (visited_articles_keys, visited_articles_vectorized), (scrapped_articles_keys, scrapped_articles_vectorized)
+
+
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from scraper import WikiArticleGetter
 from text_processor import Preprocessor
 from utils import save_wiki_articles, load_wiki_articles
+from evaluator import Evaluator
+import os
 
 if __name__ == "__main__":
-    wikiScraper = WikiArticleGetter()
-    wikiScraper.retrieve_wiki_articles(4)
+    if not os.path.exists("wiki_articles.csv"):
+        wikiScraper = WikiArticleGetter()
+        wikiScraper.retrieve_wiki_articles(4)
 
-    preprocessor = Preprocessor()
-    preprocessor.process_articles(wikiScraper.dict_of_pages)
+        preprocessor = Preprocessor()
+        preprocessor.process_articles(wikiScraper.dict_of_pages)
 
-    save_wiki_articles("wiki_articles.csv", preprocessor.processed_articles)
+        save_wiki_articles("wiki_articles.csv", preprocessor.processed_articles)
 
     wiki_articles = load_wiki_articles("wiki_articles.csv")
+
+    wikiScraper = WikiArticleGetter()
+    wikiScraper.retrieve_wiki_articles(2)
+    preprocessor = Preprocessor()
+    preprocessor.process_articles(wikiScraper.dict_of_pages)
+    query = preprocessor.processed_articles
+
+    evaluator = Evaluator(query, wiki_articles)
+    result, similarities = evaluator.evaluate()
+    print(result)
+    print(similarities)
